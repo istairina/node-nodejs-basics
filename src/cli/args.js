9e66b1156798bd65c298f093f3915b8a092f998f
Irina Migunova <ista.irina@gmail.com>
2023-06-06 17:27:43 +0300

    feat: solution for Cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argv = process.argv;
+  const res = [];
+
+  let i = 2;
+  while (i < argv.length) {
+    res.push(`${argv[i]} is ${argv[++i]}`);
+    i++;
+  }
+  console.log(res.join(", "));
 };
 
 parseArgs();

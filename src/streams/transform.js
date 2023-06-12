d8e881b7227a115e737619c8e30981c08416f4ec
Irina Migunova <ista.irina@gmail.com>
2023-06-13 00:39:29 +0300

    feat: solve Streams task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from "stream";
+
+const reverse = new Transform({
+  transform(chunk, encoding, callback) {
+    const chunkTrimmed = chunk.toString().trim().split("").reverse().join("");
+    this.push(chunkTrimmed + "\n");
+    callback();
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  process.stdin.pipe(reverse).pipe(process.stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fullPath = path.join(__dirname, "files", "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  let writeableStream = fs.createWriteStream(fullPath);
+  process.stdin.pipe(writeableStream);
 };
 
 await write();

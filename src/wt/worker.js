13bd03393c17bd9aa535d9877b5d3f38cc0edc4b
Irina Migunova <ista.irina@gmail.com>
2023-06-16 14:03:40 +0300

    fix: comment line

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,8 +3,8 @@ import { workerData, parentPort } from "worker_threads";
 const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-//comment the line below to resolve without errors
-if (Math.random() > 0.8) throw new Error();
+//uncomment the line below to see result with errors
+// if (Math.random() > 0.8) throw new Error();
 
 const sendResult = () => {
   parentPort.postMessage(nthFibonacci(workerData));

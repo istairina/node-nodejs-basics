1b54955be800f439a53fb0973b0db6727b80b5ab
Irina Migunova <ista.irina@gmail.com>
2024-01-30 01:12:51 +0300

    feat: change cp function to silent: true

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,14 +1,15 @@
 import { fork } from "child_process";
 import path from "path";
 import { fileURLToPath } from "url";
+import {stdin, stdout} from "process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const script = path.join(__dirname, "files", "script.js");
 
 const spawnChildProcess = async (args) => {
-  fork(script, args, { silent: false });
+  fork(script, args, { silent: true, stdio: [stdin, stdout, 'pipe', 'ipc'] });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess([]);
+spawnChildProcess(["hey", "hola", "ciao"]);
